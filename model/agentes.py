4f2c7cd36f21c68f6b0ec41265edfc80bc43b302
dfcastro <dfcastro@outlook.com.br>
2016-06-28 21:02:55 -0300

    model agentes

---
+++ model/agentes.py
@@ -0,0 +1,12 @@
+class agentes():
+    nome = ""
+    telefone = None
+    endereco = None
+    categoria = None
+
+    def __init__(self, nome, telefone, endereco, categoria):
+        self.nome = nome
+        self.telefone = telefone
+        self.endereco = endereco
+        self.categoria = categoria
+        pass
